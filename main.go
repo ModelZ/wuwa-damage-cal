da03971dc4868eb62df6bba0416b689072ed4968
ModelZ <model5053@gmail.com>
2025-05-19 23:29:41 +0700

    temp clean some parameter for easy to use (Can revert)

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	mzwuwacal "./mz_wuwa_cal"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Create an attacker
+	attacker := mzwuwacal.Attacker{
+		Name:               "Attacker",
+		Level:              90,
+		BaseDmg:            1000,
+		AbilityDmg:         1.5,
+		TotalAtk:           2000,
+		ElementDmgBonus:    0.2,
+		AttackTypeDmgBonus: 0.3,
+		DmgAmplify:         0.4,
+		CritDmg:            0.5,
+		DefIgnore:          0.6,
+		ElementResIgnore:   0.7,
+	}
+
+	fmt.Println("Attacker:", attacker)
+
+	// Create a target
+	target := mzwuwacal.Target{
+		Name:                 "Target",
+		Level:                90,
+		BaseElementRes:       0.1,
+		DmgTaken:             0.2,
+		DmgResBase:           0.3,
+		DmgResAdditional:     0.4,
+		ElementResBase:       0.5,
+		ElementResAdditional: 0.6,
+		DmgAmplify:           0.7,
+	}
+
+	fmt.Println("Target:", target)
 }
--- mz_wuwa_cal/mz_wuwa_cal.go
+++ mz_wuwa_cal/mz_wuwa_cal.go
@@ -2,132 +2,137 @@ package mzwuwacal
 
 type Attacker struct {
 	// Basic info of the attacker
-	name        string
-	level       int
-	element     string
-	attack_Type int
+	Name  string
+	Level int
+	// Element     string
+	// Attack_Type int
 
-	// true damage of the attacker
-	true_dmg float64
+	// True damage of the attacker
+	// TrueDmg float64
 
 	// Base damage of the attacker
-	base_dmg    int64
-	ability_dmg float64
-	// total attack of the attacker
-	total_atk     int64
-	base_atk      int64
-	atk_buff      float64
-	flat_atk_buff int64
-	flat_atk      int64
+	BaseDmg    int64
+	AbilityDmg float64
+	// Total attack of the attacker
+	TotalAtk int64
+	// BaseAtk      int64
+	// AtkBuff      float64
+	// FlatAtkBuff  int64
+	// FlatAtk      int64
 
 	// DMG Bonus of the attacker
-	bonus_multiplier      float64
-	dmg_bonus             float64
-	element_dmg_bonus     float64
-	attack_type_dmg_bonus float64
-	dmg_amplify           float64
-	special_dmg           float64
-	crit_dmg              float64
+	// BonusMultiplier float64
+	// DmgBonus             float64
+	ElementDmgBonus    float64
+	AttackTypeDmgBonus float64
+	DmgAmplify         float64
+	CritDmg            float64
+
+	// penetration multiplier
+	DefIgnore        float64 // <-> def
+	ElementResIgnore float64 // <-> base_element_res
+
+	// Unknown Source and not use in json
+	SpecialDmg float64
 }
 
 type Target struct {
 	// Basic info of the target
-	name    string
-	level   int
-	element string
+	Name  string
+	Level int
+	// Element string
 
 	// all resistances of the target
-	all_resistances    float64
-	base_element_res   float64
-	def_multiplier     float64
-	dmg_reduce         float64
-	element_dmg_reduce float64
+	// AllResistances    float64
+	BaseElementRes float64
+	// DefMultiplier     float64
+	// DmgReduce         float64
+	// ElementDmgReduce  float64
 
 	// negative resistances of the target
-	def_ignore         float64 // <-> def
-	dmg_taken          float64 // <-> dmg_reduce
-	element_res_ignore float64 // <-> base_element_res
+	DmgTaken float64 // <-> dmg_reduce
 
-	// Unknown Source
-	dmg_res_base           float64
-	dmg_res_additional     float64
-	element_res_base       float64
-	element_res_additional float64
-	dmg_amplify            float64
+	// Unknown Source and not use in json
+	DmgResBase           float64
+	DmgResAdditional     float64
+	ElementResBase       float64
+	ElementResAdditional float64
+	DmgAmplify           float64
 }
 
 // Damage Calculation
 func GetTrueDamage(a *Attacker, t *Target) float64 {
-	return GetBaseDamage(a) * GetAllResistances(a, t) * a.bonus_multiplier
+	return GetBaseDamage(a) * GetAllResistances(a, t) * GetBonusMultipliers(a, t)
 }
 
 func GetBaseDamage(a *Attacker) float64 {
-	return a.ability_dmg * GetTotalAttack(a)
+	return a.AbilityDmg * float64(a.TotalAtk)
 }
 
-func GetTotalAttack(a *Attacker) float64 {
-	return float64(a.base_atk)*(1+a.atk_buff) + float64(a.flat_atk) + float64(a.flat_atk_buff)
-}
+// No need to use this function TOO DETAILS
+// func GetTotalAttack(a *Attacker) float64 {
+// 	return float64(a.BaseAtk)*(1+a.AtkBuff) + float64(a.FlatAtk) + float64(a.FlatAtkBuff)
+// }
 
 // Resistances Calculation
 func GetAllResistances(a *Attacker, t *Target) float64 {
-	return GetBaseElementRes(t) * GetDefMultiplier(a, t) * GetDmgReduce(t) * GetElementReduce(t)
+	return GetBaseElementRes(a, t) * GetDefMultiplier(a, t) * GetDmgReduce(t) * GetElementReduce(t)
 }
 
-func GetBaseElementRes(t *Target) float64 {
+func GetBaseElementRes(a *Attacker, t *Target) float64 {
 
-	res_total := t.base_element_res - t.element_res_ignore
+	resTotal := t.BaseElementRes - a.ElementResIgnore
 
-	if res_total < 0 {
-		return 1 - (res_total / 2)
-	} else if res_total < 0.8 {
-		return 1 - res_total
-	} else { // res_total => 0.8
-		return 1 / (1 + (5 * res_total))
+	if resTotal < 0 {
+		return 1 - (resTotal / 2)
+	} else if resTotal < 0.8 {
+		return 1 - resTotal
+	} else { // resTotal => 0.8
+		return 1 / (1 + (5 * resTotal))
 	}
 
 }
 
 func GetDefMultiplier(a *Attacker, t *Target) float64 {
-	target_def := (8 * t.level) + 792
-	attacker_multiplier := float64(800 + (8 * a.level))
-	return attacker_multiplier / (attacker_multiplier + float64(target_def)*(1-t.def_ignore))
+	targetDef := (8 * t.Level) + 792
+	attackerMultiplier := float64(800 + (8 * a.Level))
+	return attackerMultiplier / (attackerMultiplier + float64(targetDef)*(1-a.DefIgnore))
 }
 
 func GetDmgReduce(t *Target) float64 {
 	// Maybe Unused I just set it as 0
-	t.dmg_res_base = 0.0
-	t.dmg_res_additional = 0.0
+	t.DmgResBase = 0.0
+	t.DmgResAdditional = 0.0
 	// I think damage taken is here
-	return 1 - (t.dmg_res_base + t.dmg_res_additional) + t.dmg_taken
+	return 1 - (t.DmgResBase + t.DmgResAdditional) + t.DmgTaken
 }
 
 func GetElementReduce(t *Target) float64 {
 	// Maybe Unused I just set it as 0
-	t.dmg_res_base = 0.0
-	t.dmg_res_additional = 0.0
-	return 1 - (t.element_res_base + t.element_res_additional)
+	t.DmgResBase = 0.0
+	t.DmgResAdditional = 0.0
+	return 1 - (t.ElementResBase + t.ElementResAdditional)
 }
 
 // Bonus Multipler Calculation
-func GetBonusMultipliers(a *Attacker) float64 {
-	return a.dmg_bonus * a.dmg_amplify * a.special_dmg * a.crit_dmg
+func GetBonusMultipliers(a *Attacker, t *Target) float64 {
+	return GetDmgBonus(a) * GetDmgAmplify(a, t) * GetSpecialDmg(a) * a.CritDmg
 }
 
 // element_dmg_bonus% -> Spectro/Aero/Glacio/Fusion/Electro/Havoc DMG bonus
 // atk_type_dmg_bonus% -> basic/heavy/reson_skill/reson_riberation DMG Bonus
 func GetDmgBonus(a *Attacker) float64 {
-	return 1 + (a.element_dmg_bonus + a.attack_type_dmg_bonus)
+	return 1 + (a.ElementDmgBonus + a.AttackTypeDmgBonus)
 }
 
 func GetDmgAmplify(a *Attacker, t *Target) float64 {
 	// Maybe Unused I just set it as 0
-	t.dmg_amplify = 0.0
-	return 1 + a.dmg_amplify + t.dmg_amplify
+	t.DmgAmplify = 0.0
+	return 1 + a.DmgAmplify + t.DmgAmplify
 }
 
 func GetSpecialDmg(a *Attacker) float64 {
 	// Maybe Unused I just set it as 0
-	a.special_dmg = 0.0
-	return 1 + a.special_dmg
+	a.SpecialDmg = 0.0
+	return 1 + a.SpecialDmg
 }
