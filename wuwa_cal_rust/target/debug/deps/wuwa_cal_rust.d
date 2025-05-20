d190346440fad5184d219097b466ecd25c474f07
ModelZ <model5053@gmail.com>
2025-05-20 19:06:40 +0700

    remove Rust ignored file

--- wuwa_cal_rust/target/debug/deps/wuwa_cal_rust.d
+++
@@ -1,6 +0,0 @@
-D:\wuwa_cal\wuwa_cal_rust\target\debug\deps\wuwa_cal_rust.exe: src\main.rs src\mz_wuwa_cal.rs
-
-D:\wuwa_cal\wuwa_cal_rust\target\debug\deps\wuwa_cal_rust.d: src\main.rs src\mz_wuwa_cal.rs
-
-src\main.rs:
-src\mz_wuwa_cal.rs:
--- wuwa_cal_rust/target/debug/wuwa_cal_rust.d
+++
@@ -1,1 +0,0 @@
-D:\wuwa_cal\wuwa_cal_rust\target\debug\wuwa_cal_rust.exe: D:\wuwa_cal\wuwa_cal_rust\src\main.rs D:\wuwa_cal\wuwa_cal_rust\src\mz_wuwa_cal.rs
